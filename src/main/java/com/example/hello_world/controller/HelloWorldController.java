8baf630a0cb555348a245577adcdafd07d02252e
Joadson Silva <joadson83@gmail.com>
2025-04-26 14:28:14 -0300

    first commit

---
+++ src/main/java/com/example/hello_world/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.example.hello_world.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/hello-world")
+public class HelloWorldController {
+
+    @GetMapping
+    public ResponseEntity<String> helloWorld() {
+        return ResponseEntity.ok("Hello World!");
+    }
+
+}
